83346991f84cd97e514332559f1bed68eafe7b15
xdsb <JYScity@163.com>
2020-08-11 00:55:35 +0800

    更新测试编译结果

--- Test/TestC.c
+++ Test/TestC.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
-main() 
+int main() 
 { 
-   printf("Hello C-world!\n"); 
-   printf(" ****\n"); 
-   printf(" *\n"); 
-   printf(" * \n"); 
-   printf(" ****\n"); 
+   char ucTest;
+   scanf("Please input a Key:%c\n",&ucTest);
+   switch(ucTest)
+   {
+     case 's': 
+          printf("Hello C-world!\n");
+          break;
+      printf(" ****\n"); 
+      printf(" *\n"); 
+      printf(" * \n"); 
+      printf(" ****\n");
+      default:
+          putchar('E'); 
+
+
+   }
+  
    return 0;
 } 
